fb049310331dc1822b60806249cc5b00e496b7e0
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-05 17:03:22 -0600

    Added members and method signatures for AuditFile class

--- Project1/src/fileio/AuditFile.java
+++ Project1/src/fileio/AuditFile.java
@@ -1,4 +1,18 @@
 package fileio;
 
 public class AuditFile {
+
+    private String filename;
+
+    public AuditFile(String filename) {
+        this.filename = filename;
+    }
+
+    public void write(String text) {
+
+    }
+
+    public String getFilename() {
+        return this.filename;
+    }
 }
