047fef3bc67e8e44914fee5c206c15a3d22c2776
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-05 17:56:16 -0600

    Removed static from line_num variables and added remaining signatures missing from UML class diagram v2

--- Project1/src/elections/OPLElection.java
+++ Project1/src/elections/OPLElection.java
@@ -6,11 +6,11 @@ import java.util.Set;
 
 public class OPLElection implements Election {
 
-    private static int num_candidate_line_num;
-    private static int num_seats_line_num;
-    private static int candidates_line_num;
-    private static int num_ballots_line_num;
-    private static int ballot_start_line_num;
+    private int num_candidate_line_num;
+    private int num_seats_line_num;
+    private int candidates_line_num;
+    private int num_ballots_line_num;
+    private int ballot_start_line_num;
     private BallotFile bf;
     private AuditFile af;
     private int num_candidates;
@@ -25,6 +25,36 @@ public class OPLElection implements Election {
     public OPLElection(BallotFile bf, AuditFile af) {
         this.bf = bf;
         this.af = af;
+        this.num_candidate_line_num = 2;
+        this.candidates_line_num = 3;
+        this.num_seats_line_num = 4;
+        this.num_ballots_line_num = 5;
+    }
+
+    public OPLElection(BallotFile bf,
+                       AuditFile af,
+                       int num_candidates_line_num,
+                       int candidates_line_num,
+                       int num_ballots_line_num,
+                       int ballot_start_line_num) {
+        this.bf = bf;
+        this.af = af;
+        this.num_candidate_line_num = num_candidates_line_num;
+        this.candidates_line_num = candidates_line_num;
+        this.num_ballots_line_num = num_ballots_line_num;
+        this.ballot_start_line_num = ballot_start_line_num;
+    }
+
+    public int getNumCandidates() {
+        return num_candidates;
+    }
+
+    public int getNumBallots() {
+        return num_ballots;
+    }
+
+    public int getQuota() {
+        return quota;
     }
 
     @Override
@@ -39,6 +69,22 @@ public class OPLElection implements Election {
 
     @Override
     public void getPartyWinners() {
+
+    }
+
+    private void populateCandidates() {
+
+    }
+
+    private void voteFor(int candidate_id, int ballot_id) {
+
+    }
+
+    private void sortPartiesOnRemainingVotes() {
+
+    }
+
+    private void sortCandidatesOnVotes() {
 
     }
 }
