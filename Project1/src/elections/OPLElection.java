569f9a0ef8d873d7ff83bcf1d28ae5b32ca42969
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-05 17:38:23 -0600

    Added OPL election class according to UML v2

--- Project1/src/elections/OPLElection.java
+++ Project1/src/elections/OPLElection.java
@@ -1,4 +1,38 @@
 package elections;
 
-public class OPLElection {
+import fileio.*;
+
+import java.util.Set;
+
+public class OPLElection implements Election {
+
+    private static int num_candidate_line_num;
+    private static int num_seats_line_num;
+    private static int candidates_line_num;
+    private static int num_ballots_line_num;
+    private static int ballot_start_line_num;
+    private BallotFile bf;
+    private AuditFile af;
+    private int num_candidates;
+    private int num_seats;
+    private int num_ballots;
+    private int quota;
+    private Candidate[] candidates;
+    private Party[] parties;
+    private Set<Candidate> winning_candidates;
+    private Set<Party> winning_parties;
+    @Override
+    public void runElection() {
+
+    }
+
+    @Override
+    public void getCandidateWinners() {
+
+    }
+
+    @Override
+    public void getPartyWinners() {
+
+    }
 }
