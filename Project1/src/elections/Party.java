2e9238501a6a73eff356aea8540e78a5f6baf309
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-05 17:22:48 -0600

    Added party class based on uml v2

---
+++ Project1/src/elections/Party.java
@@ -0,0 +1,60 @@
+package elections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Party {
+
+    private String name;
+    private int num_candidates;
+    private int num_seats;
+    private int num_votes;
+    private int num_remaining_votes;
+    private List<Integer> acquired_ballots;
+
+    public Party(String name, int num_candidates) {
+        this.name = name;
+        this.num_candidates = num_candidates;
+        this.num_seats = 0;
+        this.num_votes = 0;
+        this.num_remaining_votes = 0;
+        this.acquired_ballots = new ArrayList<Integer>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getNumCandidates() {
+        return this.num_candidates;
+    }
+
+    public int getNumSeats() {
+        return this.num_seats;
+    }
+
+    public int getNumVotes() {
+        return this.num_votes;
+    }
+
+    public int getNumRemainingVotes() {
+        return this.num_remaining_votes;
+    }
+
+    public List<Integer> getAcquiredBallots() {
+        return this.acquired_ballots;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumCandidates(int num_candidates) {
+        this.num_candidates = num_candidates;
+    }
+
+    public void acquireBallot(int ballot_id) {
+        this.num_votes += 1;
+        acquired_ballots.add(ballot_id);
+    }
+}
