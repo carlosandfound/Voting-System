2a24ee2e62f152dd0e5a2f13ecae91d472ea4a26
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-05 17:00:48 -0600

    Added members and method signatures for BallotFile class according to UML v2 diagram

--- Project1/src/fileio/BallotFile.java
+++ Project1/src/fileio/BallotFile.java
@@ -1,4 +1,21 @@
 package fileio;
 
 public class BallotFile {
+
+    private String filename;
+    private String[] data;
+
+    public BallotFile(String filename) {
+        this.filename = filename;
+
+        // TODO: read "filename" and load the data into "data"
+    }
+
+    public String getLine(int line_num) {
+        return data[line_num-1];
+    }
+
+    public String getElectionType() {
+        return getLine(1);
+    }
 }
