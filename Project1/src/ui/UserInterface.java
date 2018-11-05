85e88798204a4fc093bfe368e5b048dd8692f7ea
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-05 17:14:38 -0600

    UI can now prompt for a file name

--- Project1/src/ui/UserInterface.java
+++ Project1/src/ui/UserInterface.java
@@ -1,11 +1,31 @@
 package ui;
 
+import elections.Election;
+import fileio.*;
+import java.util.Scanner;
+
 public class UserInterface {
 
+    private Scanner reader;
+    private BallotFile bf;
+    private AuditFile af;
+    private Election e;
 
-    
+    public UserInterface() {
+        reader = new Scanner(System.in);
+        String ballot_filename = requestBallotFilename();
+    }
+
+    public String requestBallotFilename() {
+        System.out.println("Enter the ballot file you wish to use.");
+        return reader.nextLine();
+    }
+
+    public void displayResults() {
+
+    }
+
     public static void main(String[] args) {
-
-        return;
+        UserInterface ui = new UserInterface();
     }
 }
