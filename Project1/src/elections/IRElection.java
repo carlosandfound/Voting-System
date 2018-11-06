790dd0417d5317d8ba12a3331c397df939220fb8
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-05 21:25:28 -0600

    added constructor and interface methods

--- Project1/src/elections/IRElection.java
+++ Project1/src/elections/IRElection.java
@@ -1,4 +1,30 @@
 package elections;
 
-public class IRElection {
+import fileio.AuditFile;
+import fileio.BallotFile;
+
+public class IRElection implements Election{
+
+    BallotFile bf;
+    AuditFile af;
+
+    public IRElection(BallotFile bf, AuditFile af) {
+        this.bf = bf;
+        this.af = af;
+    }
+
+    @Override
+    public void runElection() {
+
+    }
+
+    @Override
+    public void getCandidateWinners() {
+
+    }
+
+    @Override
+    public void getPartyWinners() {
+
+    }
 }
