57a88d810688ee254683c1840abd5b9d239a4caa
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-05 21:26:18 -0600

    removed AuditFile, BallotFile, and e as member variables. Moved them as local variables within main() method for better organization

--- Project1/src/ui/UserInterface.java
+++ Project1/src/ui/UserInterface.java
@@ -9,38 +9,35 @@ import java.util.Scanner;
 public class UserInterface {
 
     private Scanner reader;
-    private BallotFile bf;
-    private AuditFile af;
-    private Election e;
 
     public UserInterface() {
         reader = new Scanner(System.in);
-        String ballot_filename = requestBallotFilename();
-
-        // TODO: Get the current time and use it as the audit_filename
-        String audit_filename = "TODO";
-
-
-        bf = new BallotFile(ballot_filename);
-        af = new AuditFile(audit_filename);
-
-        if (bf.getElectionType().equals("IRV")) {
-
-        } else if (bf.getElectionType().equals("OPL")) {
-            e = new OPLElection(bf, af);
-        }
     }
 
-    public String requestBallotFilename() {
+    private String requestBallotFilename() {
         System.out.println("Enter the ballot file you wish to use.");
         return reader.nextLine();
     }
 
-    public void displayResults() {
+    public void displayResults(Election e) {
 
     }
 
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
+        String ballot_filename = ui.requestBallotFilename();
+        BallotFile bf = new BallotFile(ballot_filename);
+        // TODO: Automatically generate a name for auditfile based on timestamp
+        AuditFile af = new AuditFile("TODO");
+        Election e;
+        if (bf.getElectionType().equals("OPL")) {
+            e = new OPLElection(bf, af);
+        } else if (bf.getElectionType().equals("IRV")) {
+            e = new IRElection(bf, af);
+        } else {
+            throw new IllegalStateException("Election types must be either OPL or IRV");
+        }
+        e.runElection();
+        ui.displayResults(e);
     }
 }
