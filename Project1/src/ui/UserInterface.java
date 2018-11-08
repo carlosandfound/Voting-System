cff6fb93fa45463d8f24676e25552c8364a7aaea
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-07 23:39:35 -0600

    Changed requestBallotFileName to accept a String request, as well as input and output streams (for dependency injection). Changed displayResults to accept an outputstream (for dependency injection). Implemented displayResults method.

--- Project1/src/ui/UserInterface.java
+++ Project1/src/ui/UserInterface.java
@@ -1,25 +1,51 @@
 package ui;
 
-import elections.Election;
-import elections.IRElection;
-import elections.OPLElection;
-import fileio.*;
-import java.util.Scanner;
+import elections.*;
+import java.io.*;
 
 public class UserInterface {
 
-    private Scanner reader;
-
-    public UserInterface() {
-        reader = new Scanner(System.in);
+    public String requestBallotFilename(String request, OutputStream out, InputStream in) {
+        try {
+            out.write(request.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(in));
+        String filename;
+        try {
+            filename = br.readLine();
+            return filename;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
-    public String requestBallotFilename() {
-        System.out.println("Enter the ballot file you wish to use.");
-        return reader.nextLine();
-    }
+    public void displayResults(Election e, OutputStream out) {
+        StringBuilder sb = new StringBuilder();
 
-    public void displayResults(Election e) {
+        sb.append("The winning parties are:\n");
+        for (Party p : e.getPartyWinners()) {
+            sb.append(p.getName()).append(" with ").append(p.getNumVotes()).append(" votes\n");
+        }
 
+        sb.append("\nThe winning candidates are:\n");
+        for (Candidate c : e.getCandidateWinners()) {
+            sb.append(c.getName())
+                    .append(" (")
+                    .append(c.getParty())
+                    .append(") ")
+                    .append("with ")
+                    .append(c.getNumVotes())
+                    .append(" votes\n");
+        }
+
+        String result = sb.toString();
+        try {
+            out.write(result.getBytes());
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
     }
 }
