e162c375d77a7d2d5f7d1fac8ab7ea4b843742bf
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-07 20:22:21 -0600

    Added  empty test class for UserInterface

---
+++ Project1/testing/ui/UserInterfaceTest.java
@@ -0,0 +1,21 @@
+package ui;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserInterfaceTest {
+
+    @BeforeAll
+    void setUp() {
+        
+    }
+
+    @Test
+    void requestBallotFilename() {
+    }
+
+    @Test
+    void displayResults() {
+    }
+}
