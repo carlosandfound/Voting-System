d1ee3d7ab7f60bd2eea5e889692885c7fede9695
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-07 19:58:32 -0600

    Removed main method from UI class. Moved main method to Voting System class

--- Project1/src/ui/UserInterface.java
+++ Project1/src/ui/UserInterface.java
@@ -14,30 +14,12 @@ public class UserInterface {
         reader = new Scanner(System.in);
     }
 
-    private String requestBallotFilename() {
+    public String requestBallotFilename() {
         System.out.println("Enter the ballot file you wish to use.");
         return reader.nextLine();
     }
 
     public void displayResults(Election e) {
 
-    }
-
-    public static void main(String[] args) {
-        UserInterface ui = new UserInterface();
-        String ballot_filename = ui.requestBallotFilename();
-        BallotFile bf = new BallotFile(ballot_filename);
-        // TODO: Automatically generate a name for auditfile based on timestamp
-        AuditFile af = new AuditFile("TODO");
-        Election e;
-        if (bf.getElectionType().equals("OPL")) {
-            e = new OPLElection(bf, af);
-        } else if (bf.getElectionType().equals("IRV")) {
-            e = new IRElection(bf, af);
-        } else {
-            throw new IllegalStateException("Election types must be either OPL or IRV");
-        }
-        e.runElection();
-        ui.displayResults(e);
     }
 }
