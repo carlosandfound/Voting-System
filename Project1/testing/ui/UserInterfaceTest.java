5ada97d2a901d33cee3a92a6c3e207f0a1fafee2
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-07 23:41:08 -0600

    Added tests for UserInterface methods

--- Project1/testing/ui/UserInterfaceTest.java
+++ Project1/testing/ui/UserInterfaceTest.java
@@ -1,20 +1,49 @@
 package ui;
 
-import org.junit.jupiter.api.BeforeAll;
+import elections.IRElection;
+import elections.OPLElection;
+import fileio.AuditFile;
+import fileio.BallotFile;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserInterfaceTest {
 
-    @BeforeAll
+    private OPLElection opl;
+    private IRElection ir;
+    private BallotFile bf;
+    private AuditFile af;
+    private UserInterface ui;
+
+    @BeforeEach
     void setUp() {
-        
+        ui = new UserInterface();
     }
 
     @Test
-    void requestBallotFilename() {
+    void requestBallotFilenameSuccess() {
+        String input = "simple_opl_ballot_file.csv";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        String ballot_filename = ui.requestBallotFilename("Enter a ballot filename", System.out, System.in);
+        assertEquals(ballot_filename, input);
     }
 
+    @Test
+    void requestBallotFilenameFail() {
+        String typo_input = "simples_opl_ballot_file.csv";
+        String corrected_input = "simple_opl_ballot_file.csv";
+        System.setIn(new ByteArrayInputStream(typo_input.getBytes()));
+        String ballot_filename = ui.requestBallotFilename("Enter a ballot filename", System.out, System.in);
+        assertNotEquals(ballot_filename, corrected_input);
+    }
+
+    @Disabled
     @Test
     void displayResults() {
     }
