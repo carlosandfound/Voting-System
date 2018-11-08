52e99230d5c04a627958562429bbefc16feb825a
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-07 23:37:40 -0600

    Changed instantiation of BallotFile to require a File object in the constructor

--- Project1/src/VotingSystem.java
+++ Project1/src/VotingSystem.java
@@ -2,12 +2,16 @@ import elections.*;
 import fileio.*;
 import ui.*;
 
+import java.io.File;
+import java.nio.file.FileSystems;
+
 public class VotingSystem {
 
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
-        String ballot_filename = ui.requestBallotFilename();
-        BallotFile bf = new BallotFile(ballot_filename);
+        String ballot_filename = ui.requestBallotFilename("Enter a ballot filename", System.out, System.in);
+        File ballot_file = new File(FileSystems.getDefault().getPath("src", ballot_filename).toString());
+        BallotFile bf = new BallotFile(ballot_file);
         // TODO: Automatically generate a name for auditfile based on timestamp
         AuditFile af = new AuditFile("TODO");
         Election e;
@@ -19,6 +23,6 @@ public class VotingSystem {
             throw new IllegalStateException("Election types must be either OPL or IRV");
         }
         e.runElection();
-        ui.displayResults(e);
+        ui.displayResults(e, System.out);
     }
 }
