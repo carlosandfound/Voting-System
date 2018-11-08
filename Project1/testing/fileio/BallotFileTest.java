20cae65fa622cdb3ac72f8ab9ebfeb235ea54fda
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-08 17:19:59 -0600

    Added tests for constructor concerning whether or not it throws IOException

--- Project1/testing/fileio/BallotFileTest.java
+++ Project1/testing/fileio/BallotFileTest.java
@@ -1,10 +1,13 @@
 package fileio;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
@@ -13,13 +16,34 @@ import static org.junit.jupiter.api.Assertions.*;
 class BallotFileTest {
 
     private static BallotFile bf;
+    private static File ballot_file;
 
-    @BeforeAll
-    static void setup() {
-        File ballot_file = new File(FileSystems.getDefault()
+    @BeforeEach
+    void setup() {
+        ballot_file = new File(FileSystems.getDefault()
                 .getPath("testing", "simple_opl_ballot_file.csv")
                 .toString());
-        bf = new BallotFile(ballot_file);
+        try {
+            bf = new BallotFile(ballot_file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void BallotFileConstructorDoesNotThrowExceptions() {
+        assertDoesNotThrow(() -> {
+            BallotFile _bf = new BallotFile(ballot_file);
+        });
+    }
+
+    @Test
+    void BallotFileConstructorThrowsIOException() {
+        assertThrows(IOException.class, () -> {
+            String file_path = FileSystems.getDefault().getPath("src", "doesnotexist.csv").toString();
+            File doesNotExist = new File(file_path);
+            BallotFile _bf = new BallotFile(doesNotExist);
+        });
     }
 
     @Test
@@ -50,7 +74,7 @@ class BallotFileTest {
     }
 
     @Test
-    void getLineDoesNotExist() {
+    void getLineThrowsIndexOutOfBoundsException() {
         assertThrows(IndexOutOfBoundsException.class, () -> {
             bf.getLine(bf.getNumLines() + 1);
         });
