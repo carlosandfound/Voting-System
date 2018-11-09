83157fbc9a0ab57717467af868a445917d2a6ce9
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-08 18:17:21 -0600

    Implemented continuation loop that reprompts users whenever an invalid election type is encountered or an IO exception occurs with the ballot file

--- Project1/src/VotingSystem.java
+++ Project1/src/VotingSystem.java
@@ -10,22 +10,28 @@ public class VotingSystem {
 
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
-        String ballot_filename = ui.requestBallotFilename("Enter a ballot filename", System.out, System.in);
-        File ballot_file = new File(FileSystems.getDefault().getPath("src", ballot_filename).toString());
-        try {
-            BallotFile bf = new BallotFile(ballot_file);
-            Election e;
-            if (bf.getElectionType().equals("OPL")) {
-                e = new OPLElection(bf);
-            } else if (bf.getElectionType().equals("IRV")) {
-                e = new IRElection(bf);
-            } else {
-                throw new IllegalStateException("Election types must be either OPL or IRV");
+        boolean not_valid_file = true;
+        Election el = null;
+        while (not_valid_file) {
+            String ballot_filename = ui.requestBallotFilename(System.out, System.in);
+            File ballot_file = new File(FileSystems.getDefault().getPath("src", ballot_filename).toString());
+            try {
+                BallotFile bf = new BallotFile(ballot_file);
+                if (bf.getElectionType().equals("OPL")) {
+                    el = new OPLElection(bf);
+                    not_valid_file = false;
+                } else if (bf.getElectionType().equals("IRV")) {
+                    el = new IRElection(bf);
+                    not_valid_file = false;
+                } else {
+                    ui.displayInvalidElectionType(bf.getElectionType(), System.out);
+                }
+            } catch (IOException e) {
+                // TODO: handle cases in which an IOException is thrown, e.g. when the filename entered does not exist
+                ui.displayExceptionMessage(e, System.out);
             }
-            e.runElection();
-            ui.displayResults(e, System.out);
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+        el.runElection();
+        ui.displayResults(el, System.out);
     }
 }
