61397ee8b7492172d532c5f69dc380dd4cf40a68
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-08 18:37:12 -0600

    Added comment on trying to have private interface method runElection();

--- Project1/src/elections/Election.java
+++ Project1/src/elections/Election.java
@@ -3,7 +3,7 @@ package elections;
 import java.util.Set;
 
 public interface Election {
-    void runElection();
+    void runElection(); // Want this method to be private, but Java 8 does not allow this. Java 9 does.
     Set<Candidate> getCandidateWinners();
     Set<Party> getPartyWinners();
 }
