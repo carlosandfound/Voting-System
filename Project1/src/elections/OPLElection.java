30785de7d41a88db531481eaac0f34e3043d6f06
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-08 18:38:01 -0600

    made all "line_num" variables static, added a "has_been_run" boolean member variable to prevent re running the election multiple times

--- Project1/src/elections/OPLElection.java
+++ Project1/src/elections/OPLElection.java
@@ -6,11 +6,11 @@ import java.util.Set;
 
 public class OPLElection implements Election {
 
-    private int num_candidate_line_num;
-    private int num_seats_line_num;
-    private int candidates_line_num;
-    private int num_ballots_line_num;
-    private int ballot_start_line_num;
+    private static int num_candidate_line_num = 2;
+    private static int num_seats_line_num = 3;
+    private static int candidates_line_num = 4;
+    private static int num_ballots_line_num = 5;
+    private static int ballot_start_line_num;
     private BallotFile bf;
     private int num_candidates;
     private int num_seats;
@@ -21,24 +21,11 @@ public class OPLElection implements Election {
     private Set<Candidate> winning_candidates;
     private Set<Party> winning_parties;
 
+    private boolean has_been_run;
+
     public OPLElection(BallotFile bf) {
         this.bf = bf;
-        this.num_candidate_line_num = 2;
-        this.candidates_line_num = 3;
-        this.num_seats_line_num = 4;
-        this.num_ballots_line_num = 5;
-    }
-
-    public OPLElection(BallotFile bf,
-                       int num_candidates_line_num,
-                       int candidates_line_num,
-                       int num_ballots_line_num,
-                       int ballot_start_line_num) {
-        this.bf = bf;
-        this.num_candidate_line_num = num_candidates_line_num;
-        this.candidates_line_num = candidates_line_num;
-        this.num_ballots_line_num = num_ballots_line_num;
-        this.ballot_start_line_num = ballot_start_line_num;
+        this.has_been_run = false;
     }
 
     public int getNumCandidates() {
@@ -55,16 +42,26 @@ public class OPLElection implements Election {
 
     @Override
     public void runElection() {
+        if (has_been_run) {
+            return;
+        }
 
+        has_been_run = true;
     }
 
     @Override
     public Set<Candidate> getCandidateWinners() {
+        if (!has_been_run) {
+            runElection();
+        }
         return null;
     }
 
     @Override
     public Set<Party> getPartyWinners() {
+        if (!has_been_run) {
+            runElection();
+        }
         return null;
     }
 
