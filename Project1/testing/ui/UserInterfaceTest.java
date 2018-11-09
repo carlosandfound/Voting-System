ec2e93ac0a3da18c01612e6c34c8ec15e21a335c
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-08 18:20:16 -0600

    Added tests for displayInvalidElectionType and displayExceptionMessage. Also added new utility class ConsoleOutputCapturer to capture console output for those tests.

--- Project1/testing/ui/UserInterfaceTest.java
+++ Project1/testing/ui/UserInterfaceTest.java
@@ -1,21 +1,21 @@
 package ui;
-
-import elections.IRElection;
-import elections.OPLElection;
 import fileio.BallotFile;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
+
+import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserInterfaceTest {
-
-    private OPLElection opl;
-    private IRElection ir;
-    private BallotFile bf;
     private UserInterface ui;
 
     @BeforeEach
@@ -27,7 +27,7 @@ class UserInterfaceTest {
     void requestBallotFilenameSuccess() {
         String input = "simple_opl_ballot_file.csv";
         System.setIn(new ByteArrayInputStream(input.getBytes()));
-        String ballot_filename = ui.requestBallotFilename("Enter a ballot filename", System.out, System.in);
+        String ballot_filename = ui.requestBallotFilename(System.out, System.in);
         assertEquals(ballot_filename, input);
     }
 
@@ -36,12 +36,114 @@ class UserInterfaceTest {
         String typo_input = "simples_opl_ballot_file.csv";
         String corrected_input = "simple_opl_ballot_file.csv";
         System.setIn(new ByteArrayInputStream(typo_input.getBytes()));
-        String ballot_filename = ui.requestBallotFilename("Enter a ballot filename", System.out, System.in);
+        String ballot_filename = ui.requestBallotFilename(System.out, System.in);
         assertNotEquals(ballot_filename, corrected_input);
+    }
+
+    @Test
+    void displayInvalidElectionTypeSuccess() {
+        ConsoleOutputCapturer coc = new ConsoleOutputCapturer();
+        coc.start();
+        ui.displayInvalidElectionType("CPL", System.out);
+        String capture = coc.stop();
+        assertEquals("Detected election type as CPL. Election type must be IR or OPL\n", capture);
+    }
+
+    @Test
+    void displayInvalidElectionTypeFail() {
+        ConsoleOutputCapturer coc = new ConsoleOutputCapturer();
+        coc.start();
+        ui.displayInvalidElectionType("IR", System.out);
+        String capture = coc.stop();
+        assertNotEquals("Detected election type as CPL. Election type must be IR or OPL\n", capture);
+    }
+
+    @Test
+    void displayExceptionMessageSuccess() {
+        Exception e = new Exception("This is an exception message");
+        ConsoleOutputCapturer coc = new ConsoleOutputCapturer();
+        coc.start();
+        ui.displayExceptionMessage(e, System.out);
+        String capture = coc.stop();
+        assertEquals("This is an exception message\n", capture);
+    }
+
+    @Test
+    void displayExceptionMessageFail() {
+        Exception e = new Exception("This is an exception message");
+        ConsoleOutputCapturer coc = new ConsoleOutputCapturer();
+        coc.start();
+        ui.displayExceptionMessage(e, System.out);
+        String capture = coc.stop();
+        assertNotEquals("Thiss is an exception message\n", capture);
     }
 
     @Disabled
     @Test
     void displayResults() {
+    }
+
+    public class ConsoleOutputCapturer {
+        private ByteArrayOutputStream baos;
+        private PrintStream previous;
+        private boolean capturing;
+
+        public void start() {
+            if (capturing) {
+                return;
+            }
+
+            capturing = true;
+            previous = System.out;
+            baos = new ByteArrayOutputStream();
+
+            OutputStream outputStreamCombiner =
+                    new OutputStreamCombiner(Arrays.asList(previous, baos));
+            PrintStream custom = new PrintStream(outputStreamCombiner);
+
+            System.setOut(custom);
+        }
+
+        public String stop() {
+            if (!capturing) {
+                return "";
+            }
+
+            System.setOut(previous);
+
+            String capturedValue = baos.toString();
+
+            baos = null;
+            previous = null;
+            capturing = false;
+
+            return capturedValue;
+        }
+
+        private class OutputStreamCombiner extends OutputStream {
+            private List<OutputStream> outputStreams;
+
+            public OutputStreamCombiner(List<OutputStream> outputStreams) {
+                this.outputStreams = outputStreams;
+            }
+
+            public void write(int b) throws IOException {
+                for (OutputStream os : outputStreams) {
+                    os.write(b);
+                }
+            }
+
+            public void flush() throws IOException {
+                for (OutputStream os : outputStreams) {
+                    os.flush();
+                }
+            }
+
+            public void close() throws IOException {
+                for (OutputStream os : outputStreams) {
+                    os.close();
+                }
+            }
+        }
     }
 }
