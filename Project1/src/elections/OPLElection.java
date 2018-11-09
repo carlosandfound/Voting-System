c42514e65df967a168508b1f874b89331439108a
LAPTOP-IFM006VV\Justi <bjustinkoo@gmail.com>
2018-11-08 20:31:37 -0600

    Made static members final. Renamed Set variables to follow names of accessors. Added initialization for num_candidates, num_seats, num_ballots, quota in constructor. Added initialization for candidates and parties via populateCandidatesAndParties() method. Implemented populateCandidatesAndParties(). Added accessors for num_seats, candidates, and parties.

--- Project1/src/elections/OPLElection.java
+++ Project1/src/elections/OPLElection.java
@@ -2,15 +2,18 @@ package elections;
 
 import fileio.*;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class OPLElection implements Election {
 
-    private static int num_candidate_line_num = 2;
-    private static int num_seats_line_num = 3;
-    private static int candidates_line_num = 4;
-    private static int num_ballots_line_num = 5;
-    private static int ballot_start_line_num;
+    private static final int num_candidate_line_num = 2;
+    private static final int candidates_line_num = 3;
+    private static final int num_seats_line_num = 4;
+    private static final int num_ballots_line_num = 5;
+    private static final int ballot_start_line_num = 6;
     private BallotFile bf;
     private int num_candidates;
     private int num_seats;
@@ -18,18 +21,27 @@ public class OPLElection implements Election {
     private int quota;
     private Candidate[] candidates;
     private Party[] parties;
-    private Set<Candidate> winning_candidates;
-    private Set<Party> winning_parties;
+    private Set<Candidate> candidate_winners;
+    private Set<Party> party_winners;
 
     private boolean has_been_run;
 
     public OPLElection(BallotFile bf) {
         this.bf = bf;
         this.has_been_run = false;
+        this.num_candidates = Integer.parseInt(bf.getLine(num_candidate_line_num));
+        this.num_seats = Integer.parseInt(bf.getLine(num_seats_line_num));
+        this.num_ballots = Integer.parseInt(bf.getLine(num_ballots_line_num));
+        this.quota = this.num_ballots / this.num_seats; // TODO: divide by zero error possible here
+        populateCandidatesAndParties();
     }
 
     public int getNumCandidates() {
         return num_candidates;
+    }
+
+    public int getNumSeats() {
+        return num_seats;
     }
 
     public int getNumBallots() {
@@ -38,6 +50,14 @@ public class OPLElection implements Election {
 
     public int getQuota() {
         return quota;
+    }
+
+    public Candidate[] getCandidates() {
+        return candidates;
+    }
+
+    public Party[] getParties() {
+        return parties;
     }
 
     @Override
@@ -65,8 +85,33 @@ public class OPLElection implements Election {
         return null;
     }
 
-    private void populateCandidates() {
-
+    private void populateCandidatesAndParties() {
+        this.candidates = new Candidate[this.num_candidates];
+        Map<String, Integer> party_candidate_counts = new LinkedHashMap<>(); // LinkedHashMap to preserve ordering of key insertions
+        String candidate_line = bf.getLine(candidates_line_num);
+        String[] names_and_parties = candidate_line.split("]");
+        for (int i = 0; i < names_and_parties.length; i++) {
+            String candidate = names_and_parties[i]; //, [Foster,D
+            candidate = candidate.trim();
+            candidate = candidate.substring(1); // [Foster,D
+            int opening_square_bracket_index = candidate.indexOf("[");
+            candidate = candidate.substring(opening_square_bracket_index + 1); //Foster,D
+            String[] name_and_party = candidate.split(","); //[Foster, D]
+            String name = name_and_party[0].trim();
+            String party = name_and_party[1].trim();
+            candidates[i] = new Candidate(name, party);
+            if (party_candidate_counts.containsKey(party)) {
+                party_candidate_counts.put(party, party_candidate_counts.get(party) + 1);
+            } else {
+                party_candidate_counts.put(party, 1);
+            }
+        }
+        this.parties = new Party[party_candidate_counts.size()];
+        int party_id = 0;
+        for (String party : party_candidate_counts.keySet()) {
+            parties[party_id] = new Party(party, party_candidate_counts.get(party));
+            party_id++;
+        }
     }
 
     private void voteFor(int candidate_id, int ballot_id) {
