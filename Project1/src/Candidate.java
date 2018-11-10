e4dc67d760b0bc4438f4cefa0e6a7cdfb6dc58d7
alvar357 <alvar357@umn.edu>
2018-11-09 22:40:53 -0600

    Update Candidate.java

--- Project1/src/Candidate.java
+++ Project1/src/Candidate.java
@@ -13,18 +13,13 @@ public class Candidate {
     private String name;
     private String party;
     private int num_votes;
-    private int first_place_votes;
     private List<Integer> acquired_ballots;
-    private List<Integer> acquired_first_place_ballots;
-    private List<Integer>[] irv_acquired_votes;
 
     public Candidate(String name, String party) {
         this.name = name;
         this.party = party;
         this.num_votes = 0;
-        this.first_place_votes = 0;
         acquired_ballots = new ArrayList<Integer>();
-        acquired_first_place_ballots = new ArrayList<Integer>();
     }
 
     public String getName() {
@@ -35,20 +30,14 @@ public class Candidate {
         return party;
     }
 
-    public int getNumVotes() { return num_votes; }
-
-    public int getNumFirstPlaceVotes() {
-        return first_place_votes;
+    public int getNumVotes() {
+        return num_votes;
     }
-
+    
     public List<Integer> getAcquiredBallots() {
         return acquired_ballots;
     }
-
-    public List<Integer> getAcquiredFirstPlaceBallots() {
-        return acquired_first_place_ballots;
-    }
-
+    
     public void setName(String name) {
         this.name = name;
     }
@@ -61,9 +50,4 @@ public class Candidate {
         num_votes += 1;
         acquired_ballots.add(ballot_id);
     }
-
-    /*public void acquireFirstPlaceBallot(int ballot_id) {
-        first_place_votes += 1;
-        acquired_first_place_ballots.add(ballot_id);
-    }*/
 }
