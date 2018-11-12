28ee502922fc0edf80a32c0b651a666da39b7cbc
alvar357 <alvar357@umn.edu>
2018-11-11 18:23:30 -0600

    Update IRElection.java

--- Project1/src/IRElection.java
+++ Project1/src/IRElection.java
@@ -6,11 +6,7 @@
  * Author: Carlos Alvarenga
  */
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.Random;
+import java.util.*;
 
 public class IRElection implements Election {
     private BallotFile bf;
@@ -21,6 +17,7 @@ public class IRElection implements Election {
     private int majority_criteria;
     private int minority_criteria;
     private int winning_candidate_id;
+    private boolean has_been_run;
 
     private int[][] ballots;
     private Candidate[] candidates;
@@ -28,11 +25,11 @@ public class IRElection implements Election {
     private Set<Candidate> surviving_candidates;
     private Set<Party> surviving_parties;
     private List<Integer> loser_ballot_ids;
+    private List<Integer> votes;
 
 
     public IRElection(BallotFile bf) {
         this.bf = bf;
-        this.minority_criteria = 10000;
         int num_candidates_line_num = 2;
         this.candidates_line_num = 3;
         int num_ballots_line_num = 4;
@@ -40,67 +37,87 @@ public class IRElection implements Election {
         this.num_candidates = Integer.parseInt(bf.getLine(num_candidates_line_num));
         this.num_ballots = Integer.parseInt(bf.getLine(num_ballots_line_num));
         this.majority_criteria = (num_ballots/2) + 1;
+        this.minority_criteria = majority_criteria;
         this.ballots = new int[num_ballots][num_candidates];
         this.candidates = new Candidate[num_candidates];
         this.winning_candidate_id = -1;
+        this.has_been_run = false;
         this.surviving_candidate_ids = new HashSet<Integer>();
         this.surviving_candidates = new HashSet<Candidate>();
         this.surviving_parties = new HashSet<Party>();
         this.loser_ballot_ids = new ArrayList<Integer>();
+        this.votes = new ArrayList<Integer>();
     }
 
     @Override
     public void runElection() {
-        this.populateCandidates();
-        int row = 0;
-        for (int i = ballots_start_line_num; i < num_ballots+ballots_start_line_num; i++) {
-            String ballot[] = bf.getLine(i).split(",");
-            sortBallot(ballot, row);
-            row++;
-        }
-        for (int i = 0; i < num_ballots; i++) {
-            for (int j = 0; j < num_candidates; j++) {
-                System.out.print(ballots[i][j]+" ");
+        if (!has_been_run) {
+            this.populateCandidates();
+            int ballot_id = 0;
+            for (int i = ballots_start_line_num; i < num_ballots + ballots_start_line_num; i++) {
+                String ballot[] = bf.getLine(i).split(",");
+                sortBallot(ballot_id, ballot);
+                ballot_id++;
             }
-            System.out.println(" ");
-        }
 
-        System.out.println();
-        for (int i = 0; i < num_candidates; i++) {
-            System.out.println(candidates[i].getName() + ": " + candidates[i].getNumVotes() + ", " + candidates[i].getNumVotes() +candidates[i].getAcquiredBallots());
-        }
-        System.out.println();
+            findMajority(surviving_candidate_ids);
 
-        while (!hasWinner()) {
-            System.out.println("NO WINNER............................................................................");
-            int loser_id = determineLoser();
-            minority_criteria = 10000;
-            System.out.println(loser_id);
-            surviving_candidate_ids.remove(loser_id);
-            reallocateVotes(loser_id);
-            for (int i: surviving_candidate_ids) {
-                System.out.println("NAME: " + candidates[i].getName());
-                System.out.println("VOTES: " + candidates[i].getNumVotes());
+            while (!hasWinner()) {
+                int loser_id = determineLoser();
+                minority_criteria = majority_criteria;
+                surviving_candidate_ids.remove(loser_id);
+                reallocateVotes(loser_id);
+                findMajority(surviving_candidate_ids);
             }
-        }
 
-        winning_candidate_id = surviving_candidate_ids.iterator().next();
-        surviving_candidates.add(candidates[winning_candidate_id]);
-        Party winning_party = new Party(candidates[winning_candidate_id].getParty(), 1);
-        surviving_parties.add(winning_party);
-        for (int i: surviving_candidate_ids) {
-            System.out.println("NAME OF WINNER: " + candidates[i].getName());
+            winning_candidate_id = surviving_candidate_ids.iterator().next();
+            surviving_candidates.add(candidates[winning_candidate_id]);
+            Party winning_party = new Party(candidates[winning_candidate_id].getParty(), 1);
+            surviving_parties.add(winning_party);
+            has_been_run = true;
         }
     }
 
     @Override
     public Set<Candidate> getCandidateWinners() {
+        if (!has_been_run) {
+            runElection();
+        }
         return surviving_candidates;
     }
 
     @Override
     public Set<Party> getPartyWinners() {
+        if (!has_been_run) {
+            runElection();
+        }
         return surviving_parties;
+    }
+
+    /**
+     * Method to obtain the results of an {@code IRElection} instance.
+     * @return A {@code String} containing a short description of the election parameters as well as the winner.
+     */
+    @Override
+    public String toString() {
+        if (!has_been_run) {
+            runElection();
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("Election Type: IR\n");
+        sb.append("Number of candidates: ").append(num_candidates).append("\n");
+        sb.append("Candidates: ").append(bf.getLine(3)).append("\n");
+        sb.append("Number of ballots: ").append(num_ballots).append("\n");
+        sb.append("Winning candidate: ");
+        sb.append(surviving_candidates.iterator().next().getName());
+        sb.append(" (").append(surviving_parties.iterator().next().getName()).append(")");
+        int winning_votes = surviving_candidates.iterator().next().getNumVotes();
+        sb.append(" with ").append(winning_votes);
+        if (winning_votes == 1)
+            sb.append(" vote\n");
+        else
+            sb.append(" votes\n");
+        return sb.toString();
     }
 
     public int getNumCandidates() {
@@ -124,42 +141,47 @@ public class IRElection implements Election {
             String party = candidate_split[1].replaceAll("[(,)]", "");
             candidates[i] = new Candidate(candidate_split[0], party);
             surviving_candidate_ids.add(i);
+            votes.add(0);
         }
     }
 
-    private void sortBallot(String[] ballot, int row) {
+    private void sortBallot(int ballot_id, String[] ballot) {
         int end = num_candidates - 1;
         for (int i = 0; i < num_candidates; i++) {
             if ((i >= ballot.length) || (ballot[i].equals(""))) {
-                ballots[row][end] = -1; // no rank
+                ballots[ballot_id][end] = -1; // no rank
                 end--;
             } else {
                 int rank = Integer.parseInt(ballot[i]);
-                ballots[row][rank-1] = i;
-                if (rank == 1) {
-                    voteFor(i, row);
-                }
+                ballots[ballot_id][rank-1] = i;
+                if (rank == 1) // only vote for first place candidate
+                    voteFor(ballot_id, i);
             }
         }
     }
 
-    private void voteFor(int candidates_id, int ballot_id) {
-        candidates[candidates_id].acquireBallot(ballot_id);
-        if (candidates[candidates_id].getNumVotes() >= majority_criteria) {
-            winning_candidate_id = candidates_id;
+    private void voteFor(int ballot_id, int candidate_id) {
+        candidates[candidate_id].acquireBallot(ballot_id);
+        int candidate_votes = candidates[candidate_id].getNumVotes();
+        votes.set(candidate_id, candidate_votes);
+    }
+
+    private void findMajority(Set<Integer> surviving_candidate_ids) {
+        int max_votes = Collections.max(votes);
+        if (max_votes >= majority_criteria) { // majority exists
+            winning_candidate_id = votes.indexOf(max_votes);
             surviving_candidate_ids.clear();
-            surviving_candidate_ids.add(candidates_id);
+            surviving_candidate_ids.add(winning_candidate_id);
         }
     }
 
     private int determineLoser() {
-        Set<Integer> loser_candidate_ids =  new HashSet<Integer>();
+        List<Integer> loser_candidate_ids =  new ArrayList<Integer>();
         for (int id: surviving_candidate_ids) {
             if (candidates[id].getNumVotes() < minority_criteria) {
                 minority_criteria = candidates[id].getNumVotes();
             }
         }
-        System.out.println("MIN: " + minority_criteria);
         for (int i: surviving_candidate_ids) {
             if (candidates[i].getNumVotes() == minority_criteria) {
                 loser_candidate_ids.add(i);
@@ -173,32 +195,30 @@ public class IRElection implements Election {
 
     private void reallocateVotes(int candidates_id) {
         loser_ballot_ids = candidates[candidates_id].getAcquiredBallots();
-        if (loser_ballot_ids.size() == 0) {
-
-        }
         for (int i = 0; i < loser_ballot_ids.size(); i++) {
             int loser_ballot_id = loser_ballot_ids.get(i);
             for (int j = 1; j < num_candidates; j++) {
                 int winning_cand_id = ballots[loser_ballot_id][j];
                 if (surviving_candidate_ids.contains(winning_cand_id)) {
-                    voteFor(winning_cand_id, loser_ballot_id);
+                    voteFor(loser_ballot_id, winning_cand_id);
+                    j = num_candidates;
                 }
             }
         }
-
     }
 
     private boolean hasWinner() {
-        if ((surviving_candidate_ids.size() == 1) || (winning_candidate_id != -1)) {
+        if (surviving_candidate_ids.size() == 1) {
             return true;
         }
         return false;
     }
 
-    private int randomLoser(Set<Integer> losers) {
+    private int randomLoser(List<Integer> losers) {
         Random r = new Random();
         int low = 0;
         int high = losers.size();
-        return (r.nextInt(high-low) + low);
+        int loser_id = r.nextInt(high-low) + low;
+        return (losers.get(loser_id));
     }
 }
