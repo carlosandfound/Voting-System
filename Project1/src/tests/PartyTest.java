8483a99d086fa7b2bd8e53fdb2a70d5c4cce1d7b
LAPTOP-IFM006VV\Justi <kooxx078@umn.edu>
2018-11-12 22:34:12 -0600

    Added tests for Party Class

--- Project1/src/tests/PartyTest.java
+++ Project1/src/tests/PartyTest.java
@@ -6,45 +6,74 @@
  * Authors: Justin Koo
  */
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PartyTest {
+    private static BallotFile bf;
+    private static OPLElection e;
+    private static Party p;
+
+    @BeforeEach
+    void setup() {
+        p = new Party("D", 3);
+    }
+
+    @Test
+    void partyConstructor() {
+        assertDoesNotThrow(() -> {
+            p = new Party("R", 100);
+        });
+    }
 
     @Test
     void getName() {
+        assertEquals("D", p.getName());
     }
 
     @Test
     void getNumCandidates() {
+        assertEquals(3, p.getNumCandidates());
     }
 
     @Test
     void getNumSeats() {
+        assertEquals(0, p.getNumSeats());
     }
 
     @Test
     void getNumVotes() {
+        assertEquals(0, p.getNumVotes());
     }
 
     @Test
-    void getNumRemainingVotes() {
-    }
-
-    @Test
-    void getAcquiredBallots() {
+    void getAcquiredBallotsIsEmpty() {
+        assertTrue(p.getAcquiredBallots().isEmpty());
     }
 
     @Test
     void setName() {
+        p.setName("R");
+        assertEquals("R", p.getName());
     }
 
     @Test
     void setNumCandidates() {
+        p.setNumCandidates(4);
+        assertEquals(4, p.getNumCandidates());
     }
 
     @Test
-    void acquireBallot() {
+    void acquireBallotCorrectBallot() {
+        p.acquireBallot(0);
+        assertTrue(p.getAcquiredBallots().get(0) == 0);
+    }
+
+    @Test
+    void acquireBallotCorrectNumVotes() {
+        p.acquireBallot(0);
+        assertTrue(p.getNumVotes() == 1);
     }
 }
