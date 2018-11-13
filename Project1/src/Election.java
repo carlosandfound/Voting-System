016ffe1c35c08396fb3d4b88aa6662d796013482
LAPTOP-IFM006VV\Justi <kooxx078@umn.edu>
2018-11-12 21:09:58 -0600

    Added JavaDoc comments, removed runElection() from interface

--- Project1/src/Election.java
+++ Project1/src/Election.java
@@ -1,7 +1,29 @@
+/*
+ * File: Election.java
+ *
+ * Description: this file contains the interface definition for the Election interface used in the "Voting System" product
+ *
+ * Authors: Justin Koo
+ */
+
 import java.util.Set;
 
+/**
+ * <h1>Election</h1>
+ * <h2>Purpose</h2>
+ * Requires that any object, most likely some sort of election, to implement methods to get the winners of an election.
+ */
 public interface Election {
-    void runElection(); // Want this method to be private, but Java 8 does not allow this. Java 9 does.
+
+    /**
+     * Method to obtain all candidate winners of an election.
+     * @return A {@code Set<Candidate>} containing all {@code Candidate} objects that are winners in the election.
+     */
     Set<Candidate> getCandidateWinners();
+
+    /**
+     * Method to obtain all the party winners of an election.
+     * @return A {@code Set<Party>} containing all {@code Party} objects that are winners in the election.
+     */
     Set<Party> getPartyWinners();
 }
